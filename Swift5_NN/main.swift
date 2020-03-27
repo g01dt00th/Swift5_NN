be666636056b3ebcd34c9e8159ba9aaa06eebc12
Denis Roenko <denis@roenko.ru>
2020-03-27 16:16:24 +0300

    Initial Commit

---
+++ Swift5_NN/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift5_NN
+//
+//  Created by Роенко Денис on 27.03.2020.
+//  Copyright © 2020 Denis Roenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
